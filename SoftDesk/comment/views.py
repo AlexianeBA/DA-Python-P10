110c60b122e7b52e13a00dfa5b5f917889cf7c93
AlexianeBA <barbealexiane05@gmail.com>
2024-01-17 16:47:03 +0100

    add permissions for comment and issue

--- SoftDesk/comment/views.py
+++ SoftDesk/comment/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Comment
 from .serializers import CommentSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class CommentViewSets(viewsets.ModelViewSet):
     queryset= Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
--- SoftDesk/issue/views.py
+++ SoftDesk/issue/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from issue.serializers import IssueSerializer
+from rest_framework.permissions import IsAuthenticated
 from Project.models import Project
 from .models import Issue
 # Create your views here.
 class IssueViewSets(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
+    permission_classes = [IsAuthenticated]
