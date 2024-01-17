15dc96482f285ef0ff8e15ba30ca4de7ada77e87
AlexianeBA <barbealexiane05@gmail.com>
2024-01-17 17:43:28 +0100

    add permission for contributor

--- SoftDesk/users/views.py
+++ SoftDesk/users/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import User, Contributor
 from rest_framework import viewsets
 from .serializers import UserSerializer, ContributorProjectSerializer
-from rest_framework.authentication import TokenAuthentication
 
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,4 +14,4 @@ class UserViewSet(viewsets.ModelViewSet):
 class ContributorProjectViewSet(viewsets.ModelViewSet):
     queryset = Contributor.objects.all()
     serializer_class = ContributorProjectSerializer
-   
+    permission_classes = [IsAuthenticated]
