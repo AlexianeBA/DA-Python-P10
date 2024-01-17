7b66a590e4382bdc0d2b6a1a738871bd22132cea
AlexianeBA <barbealexiane05@gmail.com>
2024-01-17 16:43:39 +0100

    add permission of project

--- SoftDesk/Project/views.py
+++ SoftDesk/Project/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from .models import Project
 from rest_framework import viewsets
 from .serializers import ProjectSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from users.models import Contributor
 # Create your views here.
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
     
