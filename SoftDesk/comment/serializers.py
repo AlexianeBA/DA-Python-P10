2c8bf820fa4dfcf04f9d8a553852071140b8ab8c
AlexianeBA <barbealexiane05@gmail.com>
2024-01-31 09:26:04 +0100

    display issue on comment

--- SoftDesk/comment/serializers.py
+++ SoftDesk/comment/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Comment
+from issue.serializers import IssueSerializer
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    issue = IssueSerializer()
+
     class Meta:
         model = Comment
-        fields = ['id', 'description', 'issue', 'author']
-    
+        fields = ["id", "description", "issue", "author"]
